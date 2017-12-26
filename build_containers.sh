0bac44abb4af76693a9d4a1d2bd03e46bdfad195
aguado_e <enzo.aguado@epitech.eu>
2017-12-26 13:22:28 +0800

    pretify build_container.sh

--- build_containers.sh
+++ build_containers.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
+YELLOW='\033[1;33m'
+RESET='\033[0m'
+
 # Build image
-docker build --tag=aguadoe/hadoop .
+echo -ne "${YELLOW}Building image ..."
+docker build --tag=aguadoe/hadoop . &> /dev/null
+echo -e " DONE${RESET}"
 
 # Create network
+echo -ne "${YELLOW}Creating network ..."
 docker network create --driver=bridge hadoop-network &> /dev/null
+echo -e " DONE${RESET}"
 
 # Start master node
-docker rm -f hadoop-master
+docker rm -f hadoop-master &> /dev/null
+echo -ne "${YELLOW}Starting namenode (master) ..."
 docker run -itd \
-                --net=hadoop-network \
-                -p 50070:50070 \
-                -p 8088:8088 \
-                --name hadoop-master \
-                --hostname hadoop-master \
-                aguadoe/hadoop
+       --net=hadoop-network \
+       -p 50070:50070 \
+       -p 8088:8088 \
+       --name hadoop-master \
+       --hostname hadoop-master \
+       aguadoe/hadoop &> /dev/null
+echo -e "DONE${RESET}"
 
 
 # Start slave nodes
@@ -22,13 +31,14 @@ NODES=6 # Number of total nodes (1 master, N - 1 slaves)
 i=1
 while [ $i -lt $NODES ]
 do
-	docker rm -f hadoop-slave$i
-	echo "start hadoop-slave$i container..."
-	docker run -itd \
-	                --net=hadoop-network \
-	                --name hadoop-slave$i \
-	                --hostname hadoop-slave$i \
-	                aguadoe/hadoop
-	i=$(( $i + 1 ))
+    docker rm -f hadoop-slave$i &> /dev/null
+    echo -ne "${YELLOW}Starting datanode ${i} (slave) ..." 
+    docker run -itd \
+	   --net=hadoop-network \
+	   --name hadoop-slave$i \
+	   --hostname hadoop-slave$i \
+	   aguadoe/hadoop &> /dev/null
+    echo -e "DONE${RESET}" 
+    let "i++"
 done 
 
